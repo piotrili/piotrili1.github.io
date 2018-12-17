a9a52f14b771e42028d99737a14ee479f4748d0e
piotrili <45821235+piotrili@users.noreply.github.com>
2018-12-17 10:22:57 +0100

    zajecia_ostatnie

--- main.js
+++ main.js
@@ -1,11 +1,57 @@
 $(document).ready(function(){
    var mymap = L.map('mymap',
-                   {center:[52.1,21.0],
-                   zoom:10,
-                   zoomControl:false,
-                   attributionControl:false}
-                   ); 
+                    {
+                     center: [52.1, 21.0], 
+                     zoom: 5,
+                     zoomControl:true,
+                     attributionControl:true
+                    }
+                    );
+      
     
-    var lyrOSM =L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');mymap.addLayer(lyrOSM);
+    var lyrORTO = L.tileLayer.wms('http://mapy.geoportal.gov.pl/wss/service/img/guest/ORTO/MapServer/WMSServer',
+                                    {
+                                        Layers: 'Raster',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'
+                                    }
+                                 );
+    
+    var lyrSOZO = L.tileLayer.wms('http://mapy.geoportal.gov.pl/wss/service/img/guest/SOZO/MapServer/WMSServer',
+                                    {
+                                        Layers: 'Raster',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'
+                                    }
+                                  );
+    var lyrWOJ = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
+                                 { 
+                                    Layers: 'wojewodztwa',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'     
+        
+        
+    }
+                                 );
+    
+    var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+    mymap.addLayer(lyrOSM);
+
+    var baseMaps = {
+        "OpenStreetMaps": lyrOSM,
+        "ORTOFOTOMAPA": lyrORTO,
+        "Mapa sozlogiczna": lyrSOZO,
+        
+                                    };
+    //lista obiektów typu checkbox
+    var overlays = { "Mapa wojewodztw": lyrWOJ
+        
+         };
+    //lista checkboxow
+    L.control.layers(baseMaps, overlays).addTo(mymap);
+    L.control.scale().addTo(mymap);
     
 });
