9f47e5711743670a921e043337fb8bd3a7274dd1
piotrili <45821235+piotrili@users.noreply.github.com>
2018-12-20 20:36:19 +0100

    koniec
    projekt zakonczony

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
    var mymap = L.map('mymap',
                     {
-                     center: [53.38, 15.37], 
-                     zoom: 5,
+                     center: [53.65, 15.6], 
+                     zoom: 11,
                      zoomControl:true,
                      attributionControl:true
                     }
@@ -26,9 +26,9 @@ $(document).ready(function(){
                                         version: '1.1.1'
                                     }
                                   );
-    var lyrWOJ = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
+    var lyrGM = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
                                  { 
-                                    Layers: 'wojewodztwa',
+                                    Layers: 'gmina_lobez',
                                         format: 'image/png',
                                         transparent: 'true',
                                         version: '1.1.1'     
@@ -36,7 +36,87 @@ $(document).ready(function(){
         
     }
                                  );
-    
+    var lyrKO = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
+                                 { 
+                                    Layers: 'kolej',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'     
+        
+        
+    }
+                                 );
+
+    var lyrSZ = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
+                                 { 
+                                    Layers: 'szkoly',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'     
+        
+        
+    }
+                                 );
+    var lyrCM = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
+                                 { 
+                                    Layers: 'cmentarze',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'     
+        
+        
+    }
+                                 );
+    var lyrTM = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
+                                 { 
+                                    Layers: 'tereny_miesz',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'     
+        
+        
+    }
+                                 );
+     var lyrTM = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
+                                 { 
+                                    Layers: 'tereny_miesz',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'     
+        
+        
+    }
+                                 );
+     var lyrWL = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
+                                 { 
+                                    Layers: 'wody_l',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'     
+        
+        
+    }
+                                 );
+    var lyrWP = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
+                                 { 
+                                    Layers: 'woda_pow',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'     
+        
+        
+    }
+                                 );
+     var lyrDR = L.tileLayer.wms('http://localhost:8095/geoserver/ATE_Workspace/wms',
+                                 { 
+                                    Layers: 'drogi',
+                                        format: 'image/png',
+                                        transparent: 'true',
+                                        version: '1.1.1'     
+        
+        
+    }
+                                 );
     var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
     mymap.addLayer(lyrOSM);
 
@@ -47,7 +127,15 @@ $(document).ready(function(){
         
                                     };
     //lista obiektów typu checkbox
-    var overlays = { "Mapa wojewodztw": lyrWOJ
+    var overlays = { "Granica gminy": lyrGM,
+                     "Bufory dla kolei": lyrKO,
+                     "Bufory dla szkół": lyrSZ,
+                    "Bufory dla cmentarzy": lyrCM,
+                    "Bufory dla terenów mieszkaniowych": lyrTM,
+                    "Bufory dla wód liniowych": lyrWL,
+                    "Bufory dla wód powierzchniowych": lyrWP,
+                    "Bufory dla dróg": lyrDR
+                    
         
          };
     //lista checkboxow
